4c32eebb13735e0ac89614e2b1b9ff3d6254c9a0
Kirill Antipov <k.antipov@haulmont.com>
2021-06-21 18:52:14 +0300

    fixed initializing listener properties in case when these properties don't set in properties file

--- cloudqueue-api/src/main/java/io/jmix/cloudqueue/configuration/QueueProperties.java
+++ cloudqueue-api/src/main/java/io/jmix/cloudqueue/configuration/QueueProperties.java
@@ -3,6 +3,7 @@ package io.jmix.cloudqueue.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
 @ConfigurationProperties(prefix = "jmix.cloudqueue")
@@ -12,10 +13,10 @@ public class QueueProperties {
     protected Listener listener;
 
     public QueueProperties(
-            @Nullable Listener listener,
+            Listener listener,
             @DefaultValue("") String queuePrefix) {
         this.queuePrefix = queuePrefix;
-        this.listener = listener;
+        this.listener = listener == null ? new Listener() : listener;
     }
 
     public String getQueuePrefix() {
@@ -35,10 +36,22 @@ public class QueueProperties {
     }
 
     public static class Listener {
+        private static final int THREAD_POOL_CORE_SIZE_DEFAULT = 5;
+        private static final int LONG_POLLING_TIMEOUT_DEFAULT = 10000;
+        private static final int WAITING_TIME_RECEIVE_REQUEST_DEFAULT = 5;
+        private static final int MAX_NUMBER_OF_MESSAGES_DEFAULT = 10;
+
         protected int threadPoolCoreSize;
         protected int longPollingTimeout;
         protected int waitingTimeReceiveRequest;
         protected int maxNumberOfMessages;
+
+        public Listener() {
+            this.threadPoolCoreSize = THREAD_POOL_CORE_SIZE_DEFAULT;
+            this.longPollingTimeout = LONG_POLLING_TIMEOUT_DEFAULT;
+            this.waitingTimeReceiveRequest = WAITING_TIME_RECEIVE_REQUEST_DEFAULT;
+            this.maxNumberOfMessages = MAX_NUMBER_OF_MESSAGES_DEFAULT;
+        }
 
         public Listener(@DefaultValue("5") int threadPoolCoreSize,
                         @DefaultValue("10000") int longPollingTimeout,
