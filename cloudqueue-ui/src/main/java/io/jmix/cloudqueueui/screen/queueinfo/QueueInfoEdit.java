719e253a50b9ae31bb460478dc588cb4ce29b7e3
Kirill Antipov <k.antipov@haulmont.com>
2021-10-18 12:47:22 +0300

    fixed #9

--- cloudqueue-ui/src/main/java/io/jmix/cloudqueueui/screen/queueinfo/QueueInfoEdit.java
+++ cloudqueue-ui/src/main/java/io/jmix/cloudqueueui/screen/queueinfo/QueueInfoEdit.java
@@ -24,9 +24,12 @@ import io.jmix.cloudqueue.entity.QueueType;
 import io.jmix.cloudqueue.utils.CreateQueueRequestBuilder;
 import io.jmix.cloudqueue.utils.QueueInfoUtils;
 import io.jmix.core.Metadata;
+import io.jmix.ui.Notifications;
 import io.jmix.ui.component.*;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Objects;
 
 @PrimaryEditorScreen(QueueInfo.class)
 @UiController("cloudqueue_QueueInfo.edit")
@@ -45,6 +48,10 @@ public class QueueInfoEdit extends StandardEditor<QueueInfo> {
     private QueueProperties queueProperties;
     @Autowired
     private ComboBox<QueueType> typeField;
+    @Autowired
+    private Notifications notifications;
+
+    private boolean ignoreCloseButton = false;
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
@@ -55,17 +62,39 @@ public class QueueInfoEdit extends StandardEditor<QueueInfo> {
 
     @Subscribe("commitAndCloseBtn")
     public void onCommitAndCloseBtnClick(Button.ClickEvent event) {
-        QueueInfo queueInfo = getEditedEntity();
-        QueueAttributes attributes = queueInfo.getQueueAttributes();
+        if (!ignoreCloseButton) {
+            QueueInfo queueInfo = getEditedEntity();
+            QueueAttributes attributes = queueInfo.getQueueAttributes();
 
-        queueManager.createQueue(new CreateQueueRequestBuilder(queueInfo.getName())
-                .fromQueueType(queueInfo.getType())
-                .withDeliveryTime(attributes.getDeliveryTime())
-                .withMaximumMessageSize(attributes.getMaximumMessageSize())
-                .withMessageRetentionPeriod(attributes.getMessageRetentionPeriod())
-                .withVisibilityTimeout(attributes.getVisibilityTimeout())
-                .withReceiveMessageWaitTime(attributes.getReceiveMessageWaitTime())
-                .build());
+            queueManager.createQueue(new CreateQueueRequestBuilder(queueInfo.getName())
+                    .fromQueueType(queueInfo.getType())
+                    .withDeliveryTime(attributes.getDeliveryTime())
+                    .withMaximumMessageSize(attributes.getMaximumMessageSize())
+                    .withMessageRetentionPeriod(attributes.getMessageRetentionPeriod())
+                    .withVisibilityTimeout(attributes.getVisibilityTimeout())
+                    .withReceiveMessageWaitTime(attributes.getReceiveMessageWaitTime())
+                    .build());
+        }
+    }
+
+    @Subscribe
+    public void onBeforeClose(BeforeCloseEvent event) {
+        ignoreCloseButton = false;
+        String name = physicalNameField.getValue();
+        if (Objects.nonNull(name)) {
+            long countOfQueuesWithTheSameName = queueManager.loadAll()
+                    .stream()
+                    .filter(q -> q.getName().equals(name))
+                    .count();
+
+            if (countOfQueuesWithTheSameName > 0) {
+                notifications.create(Notifications.NotificationType.ERROR)
+                        .withCaption("Error: you can't create Queue with name " + name + ". Queue with that name is already exist!")
+                        .show();
+                ignoreCloseButton = true;
+                event.preventWindowClose();
+            }
+        }
     }
 
     @Subscribe("nameField")
